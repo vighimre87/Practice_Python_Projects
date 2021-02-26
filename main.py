d47beabf247c3334fc7db000519fa5f44b53b871
Imre Vigh <vighimre87@gmail.com>
2021-02-26 16:41:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Character Input
+# import datetime
+# name = input('Please enter your name: ')
+# age = int(input('Please enter your age: '))
+# copy = int(input('Please enter another number: '))
+# now = int(datetime.datetime.now().year)
+# age_turning_100 = now + (100-age)
+# msg = 'Hi ' + name + ' you are going to turn 100 years old in ' + str(age_turning_100) + '.\n'
+# print(msg*copy)
+
+
+# Odd or Even
+# num = int(input('Please enter a number: '))
+# check = int(input('Please enter another number: '))
+# # if num % 4==0:
+# #   print(f'{num} is an even number and evenly divisible by {4}')
+# # elif num % 2==0:
+# #   print(f'{num} is an even number')
+# # else:
+# #     print(f'{num} is an odd number')
+# if num % check == 0:
+#   print(f'{num} is evenly divisible by {check}')
+# else:
+#   print(f'{num} is not evenly divisible by {check}')
+
+
